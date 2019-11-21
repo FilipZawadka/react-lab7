e9c7ce0c59215665852a5d090201ded28906b0dd
FilipZawadka <38583399+FilipZawadka@users.noreply.github.com>
2019-11-21 09:38:48 +0100

    Lab7Ex1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
-
-function App() {
-  return (
-    <div>
-      Empty App
-    </div>
-  );
+import React from 'react'
+class App extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        employees: [],
+        isLoading: true
+      };
+    }
+  
+    componentDidMount() {
+      this.setState({ isLoading:true});
+      fetch('http://localhost:3004/employees')
+      .then(response => response.json())
+      .then(data => this.setState({ employees:data }))
+      .then(() => {this.setState({ isLoading: false })});
+    }
+   
+    
+    render()
+    {
+      return(
+        <div>
+        
+            {this.state.isLoading ? <label>Loading...</label>:<label>Loaded {this.state.employees.length} elements: </label>}
+            <p>
+              {this.state.employees.map((emp) => <ul key={emp.id} style={emp.active ? {color: "blue"}: {color: "red"}}>{ emp.name} {emp.age} </ul>)}
+            </p>
+            
+            
+        </div>
+      )
+    }
 }
 
-export default App;
+export default App
