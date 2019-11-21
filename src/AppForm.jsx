2e4eb7e56bce57f3686fd2819da16ab18cf4c045
FilipZawadka <38583399+FilipZawadka@users.noreply.github.com>
2019-11-21 12:20:47 +0100

    Lab7Ex3

--- src/AppForm.jsx
+++ src/AppForm.jsx
@@ -4,14 +4,46 @@ class AddEmployee extends React.Component {
     constructor(props) {
         super(props);
         this.state={
-            show: false,
+            show: true,
             name: '',
             age: '',
-            contact:'',
-            isActive:''
+           contact:'',
+            isActive:'',
+            warnContact:''
         }
 
         this.handleClick = this.handleClick.bind(this);
+        this.submitHandler = this.submitHandler.bind(this);
+        }
+
+
+
+        submitHandler = (event) =>{
+            event.preventDefault();
+            //email validation regular expresion:
+            var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; 
+            
+            if(this.state.age<18)
+            {
+                if(this.state.contact<=99999999 ||this.state.contact>999999999)
+                {
+                    this.setState({warnContact:"wrong phone number"});
+                }
+                else{
+                    this.setState({warnContact:''});
+                }
+            }
+            else
+            {
+                if(re.test(this.state.contact))
+                {
+                    this.setState({warnContact:''});
+                }
+                else
+                {
+                    this.setState({warnContact:'wrong email'});
+                }
+            }
 
         }
 
@@ -38,7 +70,8 @@ class AddEmployee extends React.Component {
                 
                 <ul>Age <input type="number" name='age' value={age} onChange={this.inputChangeHandler}/></ul>
                 <ul>{(this.state.age<18)?"Parent Name":"Name"} <input type="text" name='name' value={name} onChange={this.inputChangeHandler}/></ul>
-                <ul>{(this.state.age<18)?"Parent Phone No":"Email"} <input type="text" name='contact' value={contact} onChange={this.inputChangeHandler}/></ul>
+                <ul>{(this.state.age<18)?"Parent Phone No":"Email"} {(this.state.age<18)?<input type="number" name='contact' value={contact} onChange={this.inputChangeHandler}/>
+                : <input type="text" name='contact' value={contact} onChange={this.inputChangeHandler}/>} <a style={{color: "red"}}>{this.state.warnContact}</a></ul>
                 <input type="submit" value="Submit"/>
             </form>
             <button onClick={this.handleClick}>Cancel</button>
